2cf476840813d568ebda6e5928c48e0602b94ca8
TaoYC0904 <taoyc0904@sjtu.edu.cn>
2021-06-10 19:19:36 +0800

    continuation-based shallow embedding added

--- Proof_rules_sound_CF.v
+++ Proof_rules_sound_CF.v
@@ -36,5 +36,14 @@ Inductive WP : com -> continuation -> Assertion -> Assertion -> Assertion
 
 Definition valid_smallstep (P : Assertion) (c : com) (Q R1 R2 : Assertion) : Prop :=
   forall st, Assertion_denote st P -> WP c nil Q R1 R2 st.
+  
+Inductive safe : com -> continuation -> state -> Prop :=
+  | safe_Ter1: forall st, safe CSkip nil st 
+  | safe_Ter2: forall st, safe CBreak nil st
+  | safe_Ter3: forall st, safe CCont nil st    
+  | safe_Pre: forall c k st c' k' st',
+      cstep (c, k, st) (c', k', st') ->
+      safe c' k' st' ->
+      safe c k st.
 
 (* definition small step semantics, weakest precondition, validity *)
